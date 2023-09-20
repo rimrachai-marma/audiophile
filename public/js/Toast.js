5b9de36c541af63b4465b3d8c5e8fc40a0450696
rimrachai-marma <rimrachai02@gmail.com>
2023-09-20 19:58:41 +0600

    modified toast message

--- public/js/Toast.js
+++ public/js/Toast.js
@@ -18,9 +18,15 @@ class Toast {
   #visibilityChange;
   #shouldUnPause;
 
-  constructor(options) {
+  constructor(message, options) {
     this.#toastElem = document.createElement("div");
     this.#toastElem.classList.add("toast");
+
+    this.#toastElem.innerHTML = `${message}<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path d="M5.293 6.707l5.293 5.293-5.293 5.293c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0l5.293-5.293 5.293 5.293c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414l-5.293-5.293 5.293-5.293c0.391-0.391 0.391-1.024 0-1.414s-1.024-0.391-1.414 0l-5.293 5.293-5.293-5.293c-0.391-0.391-1.024-0.391-1.414 0s-0.391 1.024 0 1.414z"></path>
+    </svg>
+    `;
+
     requestAnimationFrame(() => {
       this.#toastElem.classList.add("show");
     });
@@ -30,6 +36,22 @@ class Toast {
       this.#shouldUnPause = document.visibilityState === "visible";
     };
     this.update({ ...DEFAULT_OPTIONS, ...options });
+  }
+
+  info() {
+    this.#toastElem.classList.add("info");
+  }
+
+  success() {
+    this.#toastElem.classList.add("success");
+  }
+
+  error() {
+    this.#toastElem.classList.add("error");
+  }
+
+  warn() {
+    this.#toastElem.classList.add("warn");
   }
 
   set autoClose(value) {
@@ -73,10 +95,6 @@ class Toast {
     container.append(this.#toastElem);
     if (currentContainer == null || currentContainer.hasChildNodes()) return;
     currentContainer.remove();
-  }
-
-  set text(value) {
-    this.#toastElem.textContent = value;
   }
 
   set canClose(value) {
